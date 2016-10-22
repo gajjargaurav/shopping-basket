b4449a1355ec85bb48d95e38f6baabaf3ca1f953
Gaurav Gajjar <gajjargaurav@gmail.com>
2016-10-22 08:47:12 +0100

    test(add firt test):
    BREAKING CHANGE: Print Receipt is no longer available

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const basket = ['apples', 'apples', 'oranges', 'papayas', 'papayas','papayas', 'apples' ]
+const expect = require('expect')
+
 const discount = {
 	formula: (count, price) => count%3 === 0 ? count*price*2/3 : count*price,
 	message: 'three for the price of two'
@@ -11,26 +12,49 @@ const catalogue = [
 	{ name: 'papayas', price: 50, discount }
 ]
 
-const order = basket.reduce((prev, curr) => (prev[curr] = ++prev[curr] || 1, prev), {})
+const receipt = (basket, catalogue) => {
+	const receipt = { items: []}
+	const order = basket.reduce((prev, curr) => (prev[curr] = ++prev[curr] || 1, prev), {})
 
-const receipt = (order, catalogue) => {
-	const totalPrice = catalogue.reduce((total, item) => {
+	receipt.total = catalogue.reduce((total, item) => {
 		let itemPrice = 0
 		if(order[item.name]) {
 			if(item.discount) {
 				itemPrice = item.discount.formula(order[item.name], item.price)
-				console.log(`${item.name} x ${order[item.name]} : ${itemPrice}p`)
-				console.log(item.discount.message)
+				// console.log(`${item.name} x ${order[item.name]} : ${itemPrice}p`)
+				// console.log(item.discount.message)
+				receipt.items.push({ name: item.name, price: itemPrice, message: item.discount.message })
 			}
 			else {
 				itemPrice = order[item.name]*item.price
-				console.log(`${item.name} x ${order[item.name]}: ${itemPrice}p`)
+				// console.log(`${item.name} x ${order[item.name]}: ${itemPrice}p`)
+				receipt.items.push({ name: item.name, price: itemPrice })
 			}
 			return total + itemPrice
 		}
 		return total
 	}, 0)
-	console.log(`Total price : ${totalPrice}p`)
+
+	return receipt
+	// console.log(`Total price : ${totalPrice}p`)
 }
 
-receipt(order, catalogue)
+// Tests
+// Positive Tests
+const stub = {
+	simple: {
+		basket:['apples', 'apples', 'oranges', 'papayas', 'apples' ],
+		receipt: {
+			items: [
+				{ name: 'apples', price: 75 },
+				{ name: 'oranges', price: 30 },
+				{ name: 'papayas', price: 50, message: 'three for the price of two' }
+			],
+			total: 155
+		}
+	}
+}
+const result = receipt(stub.simple.basket, catalogue)
+
+// receipt(basket, catalogue)
+expect(result).toEqual(stub.simple.receipt)
